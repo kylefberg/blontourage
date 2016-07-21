436a252b209ca51b7be17ad5321af2e280a6ad61
Kyle Firstenberg <kylefberg@me.com>
2016-07-21 09:08:46 -0700

    got elevator working

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,15 +1,38 @@
-   $(document).ready(function(){
-      $('.slider').slider({full_width: true});
-      $('.slider').slider('next');
-      $('.slider').slider('prev');
-      $('.slider').slider('pause');
-      $('.carousel').carousel();
-      $('.carousel').carousel('next');
-      $('.carousel').carousel('next', [3]);
-      $('.carousel').carousel('prev');
-      $('.carousel').carousel('prev', [4]);
-      $('.parallax').parallax();
-    });
+ $(document).ready(function(){
+    $('.slider').slider({full_width: true});
+    $('.slider').slider('next');
+    $('.slider').slider('prev');
+    $('.slider').slider('pause');
+    $('.carousel').carousel();
+    $('.carousel').carousel('next');
+    $('.carousel').carousel('next', [3]);
+    $('.carousel').carousel('prev');
+    $('.carousel').carousel('prev', [4]);
+    $('.parallax').parallax();
+  });
+
+   $(function() {
+// navigation for the elevator function with links
+  var homeNav = document.getElementById("nav-home");
+  var showsNav = document.getElementById("nav-shows");
+  var connectNav = document.getElementById("nav-connect");
+
+
+  new Elevator({
+    element: homeNav,
+    targetElement: document.querySelector('#home'),
+  });
+
+  new Elevator({
+    element: showsNav,
+    targetElement: document.querySelector('#shows'),
+  });
+
+  new Elevator({
+    element: connectNav,
+    targetElement: document.querySelector('#connect'),
+  });
+});
 
 
 
